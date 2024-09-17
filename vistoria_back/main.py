f1a064578e6821031c95baf80e4f189629f96be9
devclaudiocassimiro <claudio.silva@jala.university>
2024-09-17 12:16:56 -0300

    feat: implements somethings

--- vistoria_back/main.py
+++ vistoria_back/main.py
@@ -3,10 +3,12 @@ from flask_cors import CORS
 import os
 from image_descriptor import process_images
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
-from reportlab.lib.styles import getSampleStyleSheet
 from io import BytesIO
 from datetime import datetime
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_CENTER, TA_LEFT
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle
+from reportlab.lib import colors
 
 app = Flask(__name__)
 CORS(app)
@@ -57,10 +59,18 @@ def upload_file():
         if observacao:
             desc['observacao'] = observacao
 
+    # Agrupa descrições por cômodo
+    rooms = {}
+    for desc in image_descriptions:
+        room = desc['room']
+        if room not in rooms:
+            rooms[room] = []
+        rooms[room].append(desc)
+
     # Gera o PDF
-    pdf_buffer = generate_pdf(image_descriptions, vistoria_info)
+    pdf_buffer = generate_styled_pdf(rooms, vistoria_info)
     
-    # Exclui as imagens da pasta 'uploads'
+    # Exclui as imagens da pasta 'uploads' após retornar o PDF
     for file_path in uploaded_files:
         os.remove(file_path)
     
@@ -72,54 +82,136 @@ def upload_file():
         mimetype='application/pdf'
     )
 
-def generate_pdf(image_descriptions, vistoria_info):
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_CENTER, TA_LEFT
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, Table, TableStyle, KeepTogether
+from reportlab.lib import colors
+from io import BytesIO
+import os
+from datetime import datetime
+
+def generate_styled_pdf(rooms, vistoria_info):
     buffer = BytesIO()
-    
-    # Cria o documento PDF
-    doc = SimpleDocTemplate(buffer, pagesize=letter)
+    doc = SimpleDocTemplate(buffer, pagesize=letter, rightMargin=30, leftMargin=30, topMargin=30, bottomMargin=18)
     styles = getSampleStyleSheet()
+
+    # Definindo estilos personalizados
+    header_style = ParagraphStyle(
+        'HeaderStyle',
+        fontSize=14,
+        fontName='Helvetica-Bold',
+        alignment=TA_CENTER,
+        spaceAfter=12,
+        textColor=colors.HexColor('#0073e6')
+    )
+
+    subheader_style = ParagraphStyle(
+        'SubHeaderStyle',
+        fontSize=12,
+        fontName='Helvetica-Bold',
+        alignment=TA_LEFT,
+        spaceAfter=10,
+        textColor=colors.HexColor('#005bb5')
+    )
+
+    normal_style = ParagraphStyle(
+        'NormalStyle',
+        fontSize=10,
+        fontName='Helvetica',
+        alignment=TA_LEFT,
+        spaceAfter=8
+    )
+
+    # Estilo de observação em itálico
+    observation_style = ParagraphStyle(
+        'ObservationStyle',
+        fontSize=10,
+        fontName='Helvetica-Oblique',
+        alignment=TA_LEFT,
+        spaceAfter=8,
+        textColor=colors.red
+    )
+
     elements = []
 
-    # Adiciona cabeçalho com as informações
-    elements.append(Paragraph(f"Vistoria de {vistoria_info['tipo_vistoria'].upper()}: {vistoria_info['nome_edificio']}", styles['Heading1']))
-    elements.append(Paragraph(f"APARTAMENTO {vistoria_info['numero_apartamento']}", styles['Heading2']))
-    elements.append(Paragraph(f"Vistoriador: {vistoria_info['nome_vistoriador']}", styles['Heading3']))
-    elements.append(Paragraph(f"Locador: {vistoria_info['locador']}", styles['Normal']))
-    elements.append(Paragraph(f"Locatário: {vistoria_info['locatario']}", styles['Normal']))
-    data_inicio_formatada = datetime.strptime(vistoria_info['data_inicio'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%d/%m/%y')
-    elements.append(Paragraph(f"Data de Início: {data_inicio_formatada}", styles['Normal']))
-    elements.append(Paragraph(f"Endereço: {vistoria_info['endereco_imovel']}", styles['Normal']))
+    # Adiciona cabeçalho com as informações de vistoria
+    elements.append(Paragraph(f"VISTORIA DE {vistoria_info['tipo_vistoria'].upper()}: {vistoria_info['nome_edificio']}", header_style))
+    elements.append(Paragraph(f"APARTAMENTO {vistoria_info['numero_apartamento']}", subheader_style))
+    elements.append(Paragraph(f"Vistoriador: {vistoria_info['nome_vistoriador']}", normal_style))
+    elements.append(Paragraph(f"Locador: {vistoria_info['locador']}", normal_style))
+    elements.append(Paragraph(f"Locatário: {vistoria_info['locatario']}", normal_style))
+    
+    data_inicio_formatada = datetime.strptime(vistoria_info['data_inicio'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%d/%m/%Y')
+    elements.append(Paragraph(f"Data de Início: {data_inicio_formatada}", normal_style))
+    elements.append(Paragraph(f"Endereço: {vistoria_info['endereco_imovel']}", normal_style))
     elements.append(Spacer(1, 12))
-
-    # Agrupa descrições por cômodo
-    rooms = {}
-    for desc in image_descriptions:
-        room = desc['room']
-        if room not in rooms:
-            rooms[room] = []
-        rooms[room].append(desc)
 
     # Adiciona conteúdo para cada cômodo
     for room, descriptions in rooms.items():
-        elements.append(Paragraph(room.upper(), styles['Heading2']))
+        # Adicionando um contêiner para o cômodo
+        elements.append(KeepTogether([
+            Paragraph(room.upper(), header_style),
+            Spacer(1, 12)
+        ]))
+
+        # Layout de grade para o conteúdo de cada cômodo
         for desc in descriptions:
-            elements.append(Paragraph(desc['description'], styles['Normal']))
-            img_path = os.path.join(app.config['UPLOAD_FOLDER'], desc['image'])
-            img = Image(img_path, width=200, height=150)
-            elements.append(img)
+            room_elements = []
+
+            # Adiciona descrição e todas as imagens lado a lado
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], desc['image'])
+            if os.path.exists(image_path) and os.path.isfile(image_path):
+                room_elements.append([
+                    Paragraph(desc['description'], normal_style),
+                    Image(image_path, width=200, height=150)
+                ])
+            else:
+                room_elements.append([
+                    Paragraph(desc['description'], normal_style),
+                    Paragraph(f"Imagem não encontrada: {desc['image']}", normal_style)
+                ])
 
             # Adiciona a observação, se existir
             if 'observacao' in desc:
-                elements.append(Paragraph(f"Observação: {desc['observacao']}", styles['Normal']))
-                elements[-1].textColor = (1, 0, 0)  # Muda a cor da observação para vermelho
+                room_elements.append([
+                    Paragraph(f"Observação: {desc['observacao']}", observation_style),
+                    Spacer(1, 12)
+                ])
 
+            # Tabela para organizar o layout como CSS Flexbox
+            table = Table(room_elements, colWidths=[250, 200])
+            table.setStyle(TableStyle([
+                ('GRID', (0, 0), (-1, -1), 0.5, colors.grey),
+                ('VALIGN', (0, 0), (-1, -1), 'TOP'),
+                ('ALIGN', (1, 0), (1, -1), 'CENTER'),
+                ('LEFTPADDING', (0, 0), (-1, -1), 8),
+                ('RIGHTPADDING', (0, 0), (-1, -1), 8)
+            ]))
+            elements.append(table)
             elements.append(Spacer(1, 12))
+
+    # Tabela de assinaturas no final
+    assinatura_data = [
+        [f"__________________________ ASS. __________________________",
+         f"{vistoria_info['locador']}      {vistoria_info['locatario']}"]
+    ]
+    assinatura_table = Table(assinatura_data, colWidths=[250, 250])
+    assinatura_table.setStyle(TableStyle([
+        ('TEXTCOLOR', (0, 0), (-1, -1), colors.HexColor('#0073e6')),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, -1), 'Helvetica-Bold'),
+        ('BOTTOMPADDING', (0, 0), (-1, -1), 12)
+    ]))
+    elements.append(Spacer(1, 24))
+    elements.append(assinatura_table)
 
     # Salva o PDF no buffer
     doc.build(elements)
     buffer.seek(0)
-    
+
     return buffer
+
 
 if __name__ == '__main__':
     os.makedirs(UPLOAD_FOLDER, exist_ok=True)
